0d72e9de7ed7e9dbfc6226971809a0bf28256eaf
Elton Rodrigues Homem <eltonrhomem@gmail.com>
2023-06-02 08:53:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const apiKey = 'AkoUyk_rkS7vfSNFXtRzY94iIS37S71WC_bT51SMNrM-G8oO0k3Y4V6pYHNQSs2u';
+const address = 'RUA LUIZ CARLOS PENA 205 IPATINGA MG';
+
+fetch(`http://dev.virtualearth.net/REST/v1/Locations?query=${address}&key=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    // const coordinates = data.resourceSets[0].resources[0].geocodePoints[0].coordinates;
+    // const latitude = coordinates[0];
+    // const longitude = coordinates[1];
+    // console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    console.log(data.resourceSets[0].resources[0].address)
+    console.log('-------coordenadas ---------------')
+    console.log(data.resourceSets[0].resources[0].geocodePoints[0].coordinates)
+  })
+  .catch(error => console.error(error));
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Instruções
+
+- rodar npm install
+
+- Para criar a chave de API para consumir o serviço de adquiriri as coordenadas pelo bing. Acesse (https://www.bingmapsportal.com). Lá você pode logar com o github. Depois de logar, clique na opção create new account e siga os passos. Depois disso, ir ao menu My Account > My Keys, clique na opção create key e siga os passos.
+
+- depois de criado a Keym copie a key e utilize-a no seu código
